24532aa7de38a1d184a7bb571ed9941d8f94a552
Bozhi YOU <youbozhi@cs.utexas.edu>
2022-05-26 13:36:05 -0500

    Offline.

--- demo.py
+++ demo.py
@@ -1,8 +1,53 @@
+import logging
+
+
+def _get_logger(name=None, *, level=logging.WARNING, fmt=logging.BASIC_FORMAT):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    ch = logging.StreamHandler()
+    ch.setFormatter(logging.Formatter(fmt))
+    logger.addHandler(ch)
+    return logger
+
+
+logger = _get_logger(
+    __name__,
+    # level=logging.DEBUG,
+    fmt=
+    '%(asctime)s [\033[1;4m%(levelname)s\033[0m %(processName)s:%(threadName)s] %(filename)s:%(lineno)s %(message)s'
+)
+
+
 import crosspy as xp
 from crosspy import numpy as np
 from crosspy import cupy as cp
 
 if __name__ == '__main__':
+    # a = xp.array([[np.full((2,2), i*2+j) for j in range(2)] for i in range(2)])
+
+    from crosspy import cpu, gpu, PartitionScheme
+    # a = xp.array(range(6), placement=[cpu(0), gpu(0), gpu(1)])
+    partition = PartitionScheme(6, default_device=cpu(0))
+    # partition[(0, 4, 5)] = cpu(0)
+    partition[1:3] = gpu(0)
+    partition[3] = gpu(1)
+    print(partition)
+    a = xp.array(range(6), partition=partition)
+    print(a)
+    print(a.devices)
+    print(a.types)
+    print()
+
+    a[0] = a[2] + a[4]
+    print(a)
+    print(a.devices)
+    print(a.types)
+    print()
+
+    b = a.to([gpu(0), cpu(0)])
+    print(b)
+    print(b.types)
+
     # array API
     x_cpu = np.array([[1, 2, 3]])
     print("numpy array", x_cpu, "on cpu")
@@ -38,9 +83,9 @@ if __name__ == '__main__':
     print("shape:", x_cross.shape)
 
     # interoperability with numpy/cupy
-    y_cpu = x_cross.to(-1)
+    y_cpu = x_cross.to(cpu(0))
     print("all to cpu", y_cpu, type(y_cpu))
-    y_gpu0 = x_cross[:1, (0, 2, 4)].to(0)
+    y_gpu0 = x_cross[:1, (0, 2, 4)].to(gpu(0))
     print("all to", y_gpu0.device, " ", y_gpu0, type(y_gpu0))
-    y_gpu1 = x_cross.to(1)
+    y_gpu1 = x_cross.to(gpu(1))
     print("all to", y_gpu1.device, " ", y_gpu1, type(y_gpu1))
